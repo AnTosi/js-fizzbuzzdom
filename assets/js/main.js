1668612d00b2dafafb951c67c2f8ac40e8944782
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-13 16:22:25 +0200

    finiti condizionali, mancano classi di stile per i condizionali

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,35 @@
 /* seleziono l'ul e gli assegno una var */
 let cont = document.getElementById("list");
 let divPad = document.querySelector("li > div")
-
-
+const zero = 0;
 /* creo loop i da 1 a 100, poi per i multipli faccio dopo */
 
 for (let i = 1; i <=100; i++) {
     console.log(i);
-    cont.innerHTML += `<li><div>${i}</div></li>`;
+    
+    if (i % 15 == 0) {
+        cont.innerHTML += `<li><div class="fizzbuzz"> fizzbuzz </div></li>`;
+
+    }
+
+    else if (i % 3 == zero)  {
+        cont.innerHTML += `<li><div class="fizz"> fizz </div></li>`;   
+    }
+
+    else if (i % 5 == 0) {
+        cont.innerHTML += `<li><div class="buzz"> buzz </div></li>`;
+    }
+
+    
+
+    else {
+        cont.innerHTML += `<li><div> ${i} </div></li>`;
+    }
+
+    // if (z = i % 5) 
 }
+
+/* metto condizioni per multipli 3 e 5 */
+// cont.innerHTML += `<li><div>${i}</div></li>`;
+
 
