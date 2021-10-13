98df1285eff1bcd547b098c157f0fbf0e71ad5fd
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-13 15:53:56 +0200

    fatto base senza condizionali

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,13 @@
 
 /* seleziono l'ul e gli assegno una var */
 let cont = document.getElementById("list");
+let divPad = document.querySelector("li > div")
 
 
 /* creo loop i da 1 a 100, poi per i multipli faccio dopo */
 
 for (let i = 1; i <=100; i++) {
     console.log(i);
-    cont.innerHTML += `<li> ${i} </li>`;
+    cont.innerHTML += `<li><div>${i}</div></li>`;
 }
 
