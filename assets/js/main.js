73596e933c47ba6cbd958bfee1ceef94079a5dc0
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-13 15:26:15 +0200

    creato loop di base (solo in console.log)

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/* metto le variabili 
+*/
+
+/* seleziono l'ul e gli assegno una var */
+let cont = document.getElementById("list");
+
+
+/* creo loop i da 1 a 100, poi per i multipli faccio dopo */
+
+for (let i = 1; i <=100; i++) {
+    console.log(i);
+}
